c7ccb9c6ebe9b68abcc27641d1987c74c85979b2
Ignacio Barrancos <ignacio.barrancos@carm.es>
2026-02-19 13:56:41 +0100

    fix: Nueva version 6.8.2 Entrego la nueva versión liberada por el ministerio en Enero de 2026

--- fuentes/eeutil-afirmaws-stub/src/main/java/es/mpt/dsic/inside/dssprocessing/impl/XMLDetachedDSSSignerProcessor.java
+++ fuentes/eeutil-afirmaws-stub/src/main/java/es/mpt/dsic/inside/dssprocessing/impl/XMLDetachedDSSSignerProcessor.java
@@ -1,12 +1,12 @@
 /*
- * Copyright (C) 2012-13 MINHAP, Gobierno de España This program is licensed and may be used,
- * modified and redistributed under the terms of the European Public License (EUPL), either version
- * 1.1 or (at your option) any later version as soon as they are approved by the European
- * Commission. Unless required by applicable law or agreed to in writing, software distributed under
- * the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
- * either express or implied. See the License for the specific language governing permissions and
- * more details. You should have received a copy of the EUPL1.1 license along with this program; if
- * not, you may find it at http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
+ * Copyright (C) 2025, Gobierno de España This program is licensed and may be used, modified and
+ * redistributed under the terms of the European Public License (EUPL), either version 1.1 or (at
+ * your option) any later version as soon as they are approved by the European Commission. Unless
+ * required by applicable law or agreed to in writing, software distributed under the License is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied. See the License for the specific language governing permissions and more details. You
+ * should have received a copy of the EUPL1.1 license along with this program; if not, you may find
+ * it at http://joinup.ec.europa.eu/software/page/eupl/licence-eupl
  */
 
 package es.mpt.dsic.inside.dssprocessing.impl;
@@ -50,7 +50,7 @@ public class XMLDetachedDSSSignerProcessor extends XMLDSSSignerProcessor {
 
     ContenidoFirmado contenidoFirmado = new ContenidoFirmado();
 
-    // Obtenemos la expresión xpath del nodo que tiene el contenido firmado.
+    // Obtenemos la expresion xpath del nodo que tiene el contenido firmado.
     String xpathExpr = DSSUtil.expresionXpathValida(signedDataRefConEncodingOrFirst.getXPath());
 
     // Obtenemos la estructura DOM del XML de la firma
@@ -120,8 +120,9 @@ public class XMLDetachedDSSSignerProcessor extends XMLDSSSignerProcessor {
       encoding = XMLUtil.getEncoding(nodoContenido);
     }
     if (encoding == null) {
-      logger.info(
-          "No se ha podido obtener el Encoding ni del nodo contenido ni del nodo signedDataRef");
+      logger.warn(
+          "No se ha podido obtener el Encoding ni del nodo contenido ni del nodo signedDataRef, asignamos el encoding del documento"
+              + encod);
       encoding = encod;
     }
     return encoding;
